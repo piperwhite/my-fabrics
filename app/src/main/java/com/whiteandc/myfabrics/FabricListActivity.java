25f2fa20b967768cdce9962463b2c9581b72158d
Blanca Gomez Andrades <blanca_piper@hotmail.com>
2018-03-25 20:16:54 -0700

    FabricListActivity with RecyclerView

--- app/src/main/java/com/whiteandc/myfabrics/FabricListActivity.java
+++ app/src/main/java/com/whiteandc/myfabrics/FabricListActivity.java
@@ -1,13 +1,25 @@
 package com.whiteandc.myfabrics;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FabricListActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv_fabrics)
+    RecyclerView rvFabrics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fabric_list);
+        ButterKnife.bind(this);
+
+        rvFabrics.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
