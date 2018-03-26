6f63233e4a16fcc550ec42e9d224db678ac8dd67
Blanca Gomez Andrades <blanca_piper@hotmail.com>
2018-03-25 18:54:54 -0700

    First commit

---
+++ app/src/main/java/com/whiteandc/myfabrics/FabricListActivity.java
@@ -0,0 +1,13 @@
+package com.whiteandc.myfabrics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FabricListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fabric_list);
+    }
+}
